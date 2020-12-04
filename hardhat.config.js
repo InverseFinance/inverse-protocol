6b12554294ed7ed75af37f3690df614801311511
Nour Haridy <nourharidy@gmail.com>
2020-12-04 15:48:06 +0200

    add test in prod

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,18 @@
 require("@nomiclabs/hardhat-waffle");
+require('dotenv').config()
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
+  networks: {
+    hardhat: {
+      forking: {
+        url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_KEY}`,
+        blockNumber: 11386168
+      }
+    }
+  },
   solidity: {
     compilers: [ {
       version: "0.7.3"
