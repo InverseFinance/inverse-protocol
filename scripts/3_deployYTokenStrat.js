93ffaded5b89a9a72b2cce581d46ffa04dc074f0
Nour Haridy <nourharidy@gmail.com>
2020-12-15 18:29:17 +0200

    add deployment script

---
+++ scripts/3_deployYTokenStrat.js
@@ -0,0 +1,23 @@
+const prompt = require('async-prompt');
+
+async function main() {
+
+    const vault = await prompt('Enter vault address: ');
+    const yToken = await prompt('Enter yToken token address: ');
+
+    const Strat = await ethers.getContractFactory("YTokenStrat");
+    const strat = await Strat.deploy(
+      vault,
+      yToken
+    );
+    await strat.deployed();
+  
+    console.log("YTokenStrat deployed to:", strat.address);
+  }
+  
+  main()
+    .then(() => process.exit(0))
+    .catch(error => {
+      console.error(error);
+      process.exit(1);
+    });
